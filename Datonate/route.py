2d843a8a57ede32ef322d75e60d5715455e534bc
Vineel Guntupalli <Vineel@Vineels-MacBook-Pro.local>
2017-11-28 23:50:52 -0500

    Create Experiment

--- Datonate/route.py
+++ Datonate/route.py
@@ -2,8 +2,11 @@
 from flask import Flask, Response, request, render_template, url_for, redirect
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 import urllib2
+import urllib
+#import urllib.request
 import json
 import requests
+from urlparse import urlparse
 
 SERVER_ADDRESS = "http://datonate.com:5000/"
 
@@ -41,6 +44,7 @@ def login():
         else:
             print request.get_data()
             argArray = json.loads(request.data)
+
     username = argArray.get("email")
     password = argArray.get("password")
     if(username is None or password is None or len(username) < 1 or len(password) < 1 ):
@@ -93,7 +97,32 @@ def viewexp():
 @app.route("/create")
 @login_required
 def create():
-    return render_template("create.html", value=current_user.id)
+    if request.method == "GET":
+        argArray = request.args
+    elif request.method  == "POST":
+        if(len(request.form) > 0):
+            argArray = request.form
+        else:
+            print request.get_data()
+            argArray = json.loads(request.data)
+    values = {'dataset_id' : argArray.get("dataset_id"),
+              'batchSize' : argArray.get("batchSize"),
+              'price' : argArray.get("price") ,
+              'description' : argArray.get("description"),
+              'gender' :  argArray.get("gender") }
+    data=urllib.urlencode(values)
+    data=data.encode('utf-8')
+    response=urllib.urlopen(SERVER_ADDRESS+"api/" + str(current_user.id) + "/create",data)
+    jsonString=response.read()
+    #data = data.encode('ascii') # data should be bytes
+    #jsonString = urllib2.urlopen(SERVER_ADDRESS+"api/" + str(current_user.id) + "/create", data).read()
+    #jsonString = urllib.request.urlopen(req).read()
+    jsonArr = json.loads(jsonString)
+    if(jsonArr['status'] == 200):
+        return render_template("create.html", value = str(current_user.id), succ = jsonArr)
+    else:
+        return render_template("create.html", value = str(current_user.id), fail = jsonArr)
+
 if __name__ == '__main__':
     print urllib2.urlopen(SERVER_ADDRESS+"api/getUser/1").read()
 
